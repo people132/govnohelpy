0f96af8ed67dd4e002a55ab52eef30987e8390f2
people132 <kuprvvvv@gmail.com>
2022-11-07 00:00:57 +0300

    ready qu and start account

--- login.py
+++ login.py
@@ -1,66 +1,128 @@
 from flask import Flask, request, render_template, session, url_for, redirect
 import sqlite3
 import hashlib
+import os
 
 app = Flask(__name__, template_folder='pages')
+app.secret_key = os.urandom(20).hex()
 
 
-@app.route('/authorization', methods=['GET', 'POST'])
-def form_authorization():
-
-   if request.method == 'POST':
-       Login = request.form.get('Login')
-       Password = request.form.get('Password')
-#       session['login'] = Login
-
-
-       db_lp = sqlite3.connect('bases/login_password.db')
-       cursor_db = db_lp.cursor()
-       cursor_db.execute(('''SELECT password FROM login_password
+@app.route('/qu', methods=['GET', 'POST'])
+def qu():
+    if 'email' in session:
+        print(session['email'])
+        db_lp = sqlite3.connect('bases/login_password.db')
+        cursor_db = db_lp.cursor()
+        try:
+            log = cursor_db.execute(('''SELECT fio FROM info
                                                WHERE login = '{}';
-                                               ''').format(Login))
-       pas = cursor_db.fetchall()
+                                               ''').format(session['email'])).fetchone()[0]
+        except:
+            return redirect(url_for('reg'))
+        if request.method == 'POST':
+            mainSkill = request.form['select1']
+            mainHour = request.form['select2']
+            secondSkill = request.form['select3']
+            secondHour = request.form['select4']
+            hour = request.form['select5']
+            lengug = ' '.join(request.form.getlist('len'))
+            work = ' '.join(request.form.getlist('field'))
 
-       cursor_db.close()
-       try:
-           if pas[0][0] != Password:
-               return render_template('auth_bad.html')
-       except:
-           return render_template('auth_bad.html')
+            db_lp = sqlite3.connect('bases/login_password.db')
+            cursor_db = db_lp.cursor()
+            sql_insert1 = f'''UPDATE info SET mainskill = "{mainSkill}",mainhoure = "{mainHour}",secondskill = "{secondSkill}",
+                    secondhour = "{secondHour}", laguage = "{lengug}", freehour = "{hour}", work = "{work}" 
+                         WHERE login ="{session['email']}";'''
 
-       db_lp.close()
-       return render_template('successfulauth.html')
+            cursor_db.execute(sql_insert1)
 
-   return render_template('authorization.html')
+            cursor_db.close()
 
-@app.route('/registration', methods=['GET', 'POST'])
-def form_registration():
+            db_lp.commit()
+            db_lp.close()
+            print(mainSkill, mainHour, secondSkill, secondHour, lengug, work)
 
-   if request.method == 'POST':
-       Login = request.form.get('Login')
-       Password = request.form.get('Password')
-       Phone = request.form.get('Phone')
-       FIO = request.form.get('FIO')
-       Password1 = hashlib.sha512(Password.encode()).hexdigest()
-
-       db_lp = sqlite3.connect('bases/login_password.db')
-       cursor_db = db_lp.cursor()
-       sql_insert = '''INSERT INTO login_password VALUES('{}','{}');'''.format(Login, Password1)
-       sql_insert1 = '''INSERT INTO info VALUES('{}','{}', '{}');'''.format(Login, FIO, Phone)
-
-
-       cursor_db.execute(sql_insert)
-       cursor_db.execute(sql_insert1)
-
-       cursor_db.close()
-
-       db_lp.commit()
-       db_lp.close()
-
-       return render_template('successfulregis.html')
-
-   return render_template('registration.html')
+        return render_template('qu.html', log=log)
+    else:
+        return redirect(url_for('auth'))
 
 
-if __name__ == "__main__":
- app.run()
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+        session['email'] = request.form['email']
+        email = request.form['email']
+        print(email)
+        Password = request.form['password']
+        db_lp = sqlite3.connect('bases/login_password.db')
+        cursor_db = db_lp.cursor()
+        cur = cursor_db.execute(f'''SELECT password FROM login_password
+                                            WHERE login = "{email}"''').fetchone()
+        cur1 = cursor_db.execute(f'''SELECT password FROM login_password
+                                            WHERE login = "{email}"''').fetchone()
+        print(cur1)
+        print(cur)
+        if cur1 is None:
+            print('reg')
+            db_lp.close()
+            redirect(url_for('reg'))
+        else:
+            print(cur[0], hashlib.sha512(Password.encode()).hexdigest())
+            if cur[0] != str(hashlib.sha512(Password.encode()).hexdigest()):
+                db_lp.close()
+                return redirect(url_for('reg'))
+            else:
+                db_lp.close()
+                redirect(url_for('qu'))
+
+
+        return redirect(url_for('qu'))
+
+    return render_template('auth.html')
+
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        phone = request.form['phone']
+        fio = request.form['fio']
+
+        db_lp = sqlite3.connect('bases/login_password.db')
+        cursor_db = db_lp.cursor()
+        if cursor_db.execute(f'''SELECT password FROM login_password
+                                            WHERE login = "{login}"''').fetchone() == None:
+            sql_insert = f'''INSERT INTO login_password VALUES('{login}','{str(hashlib.sha512(password.encode()).hexdigest())}');'''
+            sql_insert1 = f'''INSERT INTO info VALUES('{login}','{fio}', '{phone}', '{None}', '{None}', '{None}', '{None}', '{None}', '{None}', '{None}');'''
+
+            cursor_db.execute(sql_insert)
+            cursor_db.execute(sql_insert1)
+
+            cursor_db.close()
+
+            db_lp.commit()
+            db_lp.close()
+        else:
+            return redirect(url_for('auth'))
+            return '''<script> 
+                alert('Пользователь уже существует')
+            </script>'''
+        return redirect(url_for('auth'))
+
+    return render_template('reg.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+#    print(session['email'])
+    return redirect(url_for('auth'))
+
+
+#работа над новой страницей
+#@app.route('/account')
+#def account():
+
+
+app.run(debug=True)
