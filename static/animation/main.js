2ac0bf49a7c16ea5a134147012bb5788f6e9e2d3
TEaMeNT <aleksandrovv40@gmail.com>
2022-12-17 22:47:09 +0300

    222

---
+++ static/animation/main.js
@@ -0,0 +1,33 @@
+console.log("ПРИВ");
+
+const animItems = document.querySelectorAll('._anim_items');
+console.log(animItems.length);
+if(animItems.length > 0){
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll(){
+        for(let i = 0; i < animItems.length; i++){
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+            
+            let animItemPointer = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight){
+                animItemPointer = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((window.pageYOffset > (animItemOffset - animItemPointer)) && (window.pageYOffset < (animItemOffset + animItemHeight))){
+                animItem.classList.add("_active");
+            }
+            else {
+                animItem.classList.remove("_active");
+            }
+        }
+    }
+    function offset(el) {
+        var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+}
