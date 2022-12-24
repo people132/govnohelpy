e0d100448d5556c6c33e8b379c8f4cdf53322c08
TEaMeNT <aleksandrovv40@gmail.com>
2022-12-25 02:31:32 +0300

    absolutely_new_account

---
+++ static/animation/main.js
@@ -0,0 +1,35 @@
+console.log("ПРИВ");
+
+const animItems = document.querySelectorAll(".anim_items");
+console.log(animItems.length);
+if(animItems.length > 0){
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll(){
+        for(let i = 0; i < animItems.length; i++){
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+            
+            let animItemPointer = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight){
+                animItemPointer = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((window.pageYOffset > (animItemOffset - animItemPointer)) && (window.pageYOffset < (animItemOffset + animItemHeight))){
+                animItem.classList.add("_active");
+            }
+        }
+    }
+    function offset(el) {
+        var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    animOnScroll();
+}
+
+function Changename(fieldName, text) {
+    fieldName.textContent = text;
+}
