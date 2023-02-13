8d0f7f8acc216a3a59ba6a89f5598d182d4e030d
Vladimir <kuprvvvv@gmail.com>
2023-02-13 14:36:21 +0300

    лпаро

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import random
 
 app = Flask(__name__, template_folder='pages')
 app.secret_key = os.urandom(20).hex()
+
 @app.route('/qu', methods=['GET', 'POST'])
 def qu():
     if 'email' in session:
@@ -17,9 +18,11 @@ def qu():
         db_lp = sqlite3.connect('bases/login_password.db')
         cursor_db = db_lp.cursor()
         try:
-            log = cursor_db.execute(('''SELECT fio FROM info
-                                               WHERE login = '{}';
+            log1 = cursor_db.execute(('''SELECT name FROM info
+                                               WHERE email = '{}';
                                                ''').format(session['email'])).fetchone()[0]
+            log2 = cursor_db.execute(('''SELECT surname FROM info WHERE email = '{}'; ''').format(session['email'])).fetchone()[0]
+            log = log1 + ' ' + log2
         except:
             return redirect(url_for('reg'))
         if request.method == 'POST':
@@ -34,7 +37,7 @@ def qu():
             cursor_db = db_lp.cursor()
             sql_insert1 = f'''UPDATE info SET mainSkill = "{mainSkill}",mainHour = "{mainHour}",secondSkill = "{secondSkill}",
                     secondHour = "{secondHour}", language = "{language}", workHoure = "{workHoure}", typeConnect = "{typeConnect}" 
-                         WHERE login ="{session['email']}";'''
+                         WHERE email ="{session['email']}";'''
             cursor_db.execute(sql_insert1)
             cursor_db.close()
             db_lp.commit()
@@ -71,27 +74,15 @@ def auth():
                     return redirect(url_for('reg'))
                 else:
                     skill = cursor_db.execute(('''SELECT mainSkill FROM info
-                                                WHERE login = '{}';
+                                                WHERE email = '{}';
                                                 ''').format(session['email'])).fetchone()[0]
+                    print(skill)
+                    print(type(skill))
                     db_lp.close()
             if skill != '0':
                 return  redirect(url_for('account'))
             else:
-                db_lp1 = sqlite3.connect('bases/login_password.db')
-                db_cursor1 = db_lp1.cursor()
-                select = db_cursor1.execute(('''SELECT accept FROM info WHERE login = '{}';''').format(session['email'])).fetchone()[0]
-                if select == '0':
-                    message = f'''From: From Person <>\n
-                    To: To Person <{session['email']}>\n
-                    Subject: Accept\n
-                    accept: http://127.0.0.1:5000/accept\n
-                    '''
-                    #letter(session['email'], "Активируйте аккаунт: http://127.0.0.1:5000/accept")
-                    with smtplib.SMTP_SSL("smtp.gmail.com") as smtp:
-                        smtp.login('kuprvvv@gmail.com', 'gqrl dbwn gfjf gpmb') #https://myaccount.google.com/apppasswords
-                        smtp.sendmail('kuprvvv@gmail.com', session['email'], msg = message)
-                else:
-                    return redirect(url_for('qu'))
+                return redirect(url_for('qu'))
         except UnboundLocalError:
             return redirect(url_for('reg'))
     return render_template('auth.html')
@@ -99,28 +90,69 @@ def auth():
 
 @app.route('/reg', methods=['GET', 'POST'])
 def reg():
+    error = 'error'
+    error_msg = ''
+    code = ''
+    surname = ''
+    name = ''
+    patronymic = ''
+    email = ''
+    number = ''
+    tg = ''
+    vk = ''
+    password = ''
     if request.method == 'POST':
-        surname = request.form['surname']
-        name = request.form['username']
-        patronymic = request.form['patronymic']
-        email = request.form['email']
-        number = request.form['number']
-        tg = request.form['tg']
-        vk = request.form['vk']
-        password = request.form['password']
-        db_lp = sqlite3.connect('bases/login_password.db')
-        cursor_db = db_lp.cursor()
-        if cursor_db.execute(f'''SELECT password FROM login_password
-                                            WHERE login = "{login}"''').fetchone() == None:
-            sql_insert = f'''INSERT INTO login_password VALUES('{login}','{str(hashlib.sha512(password.encode()).hexdigest())}');'''
-            sql_insert1 = f'''INSERT INTO info VALUES('{login}','{fio}', '{phone}', '{0}', '{0}', '{0}', '{0}', '{0}', '{0}', '{0}', '{0}');'''
-            cursor_db.execute(sql_insert)
-            cursor_db.execute(sql_insert1)
-            cursor_db.close()
-            db_lp.commit()
-            db_lp.close()
-        return redirect(url_for('auth'))
-    return render_template('reg.html')
+        if request.form['button'] == 'reg':
+            surname = request.form['surname']
+            name = request.form['username']
+            patronymic = request.form['patronymic']
+            email = request.form['email']
+            number = request.form['number']
+            tg = request.form['tg']
+            vk = request.form['vk']
+            code = request.form['code']
+            password = request.form['password']
+            try:
+                print(code, session['code'])
+                if session['code'] == code:
+                    session.pop('code', None)
+                    db_lp = sqlite3.connect('bases/login_password.db')
+                    cursor_db = db_lp.cursor()
+                    print('yes')
+                    if cursor_db.execute(f'''SELECT password FROM login_password
+                                                    WHERE login = "{email}"''').fetchone() == None:
+                        sql_insert = f'''INSERT INTO login_password VALUES('{email}','{str(hashlib.sha512(password.encode()).hexdigest())}');'''
+                        sql_insert1 = f'''INSERT INTO info VALUES('{email}','{name}', '{surname}', '{patronymic}', '{number}', '{vk}', '{tg}', '{0}', '{0}', '{0}', '{0}', {0}, {0}, {0}, {0});'''
+                        cursor_db.execute(sql_insert)
+                        cursor_db.execute(sql_insert1)
+                        cursor_db.close()
+                        db_lp.commit()
+                        db_lp.close()
+                        return redirect(url_for('auth'))
+                    else:
+                        print('no')
+                        error = 'error active'
+                        error_msg = 'Такой аккаунт уже существует'
+                else:
+                    error = 'error active'
+                    error_msg = 'Код введен не верно'
+                    session.pop('code', None)
+            except KeyError:
+                error = 'error active'
+                error_msg = 'отправьте новый код на почту'
+        if request.form['button'] == 'code':
+            session.pop('code', None)
+            surname = request.form['surname']
+            name = request.form['username']
+            patronymic = request.form['patronymic']
+            email = request.form['email']
+            number = request.form['number']
+            tg = request.form['tg']
+            vk = request.form['vk']
+            password = request.form['password']
+            session['code'] = generate(5)
+            letter(email, session['code'])
+    return render_template('reg.html', Error_msg = error_msg, Error = error, Surname = surname, Username = name, Patronymic = patronymic, Email = email, Number = number, Tg = tg, Vk = vk, Password = password)
 
 
 @app.route('/logout')
@@ -135,9 +167,14 @@ def account():
         summ = 0
         db_lp = sqlite3.connect('bases/login_password.db')
         cursor_db = db_lp.cursor()
-        Log = cursor_db.execute(('''SELECT fio FROM info
-                                               WHERE login = '{}';
+        Log1 = cursor_db.execute(('''SELECT name FROM info
+                                               WHERE email = '{}';
                                                ''').format(session['email'])).fetchone()[0]
+        Log2 = cursor_db.execute(('''SELECT surname FROM info
+                                               WHERE email = '{}';
+                                               ''').format(session['email'])).fetchone()[0]
+        Log = Log1 + " " + Log2
+        
 
         #Получаю из базы список email-ов
         Des = cursor_db.execute('''SELECT email FROM history;''').fetchall()
@@ -192,14 +229,25 @@ def newpassword():
         print(session['code'], code1)
         if code1 == session['code']:
             print('yes')
+            session.pop('code', None)
             return redirect(url_for('qu'))
         else:
+            session.pop('code', None)
             print('вы мошеник')
     return render_template('code.html')
 
 
-@app.route('/sent')
+@app.route('/sent_accept')
+def sent_accept():
+    session.pop('code', None)
+    code = generate(5)
+    letter(session['email'], code)
+    session['code'] = code
+    return redirect(url_for('reg'))
+
+@app.route('/sentps')
 def sent():
+    session.pop('code', None)
     code = generate(5)
     letter('kuprvvv@gmail.com', code)
     session['code'] = code
@@ -214,22 +262,6 @@ def generate(length):
     code = "".join(random.sample(Use_for, length_for_pass))
     return code
 
-
-@app.route('/accept')
-def accept():
-    if 'email' in session:
-        print(session['email'])
-        if request.method == 'POST':
-            db_lp = sqlite3.connect('bases/login_password.db')
-            cursor_db = db_lp.cursor()
-            sql_insert = f'''UPDATE info SET accept = "{1}"
-                         WHERE login ="{session['email']}";'''
-            cursor_db.execute(sql_insert)
-            cursor_db.close()
-            db_lp.close
-    else:
-        print('NULL')
-    return render_template('accept.html')
 
 def letter(email, ms):
     with smtplib.SMTP_SSL("smtp.gmail.com") as smtp:
