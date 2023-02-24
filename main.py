6bf4ea5a77cfdd08ef4e7cd8f8676efb8737e6d0
Vladimir <kuprvvvv@gmail.com>
2023-02-24 17:18:30 +0300

    dsagf

--- main.py
+++ main.py
@@ -505,8 +505,9 @@ def logout():
 
 @app.route('/account')
 def account():
+    fio = ''
+    summ = 0
     if 'email' in session:
-        summ = 0
         db_lp = sqlite3.connect('bases/login_password.db')
         cursor_db = db_lp.cursor()
         Log1 = cursor_db.execute(('''SELECT name FROM info
@@ -515,15 +516,57 @@ def account():
         Log2 = cursor_db.execute(('''SELECT surname FROM info
                                                WHERE email = '{}';
                                                ''').format(session['email'])).fetchone()[0]
-        Log = Log1 + " " + Log2
+        fio = Log1 + ' ' + Log2
 
         # Получаю из базы список email-ов
         Des = cursor_db.execute('''SELECT email FROM history;''').fetchall()
-        des3 = []
+        Des_task = cursor_db.execute('''SELECT email FROM task;''').fetchall()
+        des_hi = []
+        des4 = []
         # прохожу по списку email-ов
         for i in Des:
-            des3.append(i[0])
-        for i in set(des3):
+            des_hi.append(i[0])
+        for i in Des_task:
+            des4.append(i[0])
+        
+        for i in set(des4):
+            Full_name = cursor_db.execute(('''SELECT full_name FROM task WHERE email = '{}';''').format(i)).fetchall()
+            Des1 = cursor_db.execute(('''SELECT status FROM task WHERE email = '{}';''').format(i)).fetchall()
+            Des2 = cursor_db.execute(('''SELECT name FROM task WHERE email = '{}';''').format(i)).fetchall()
+            Des3 = cursor_db.execute(('''SELECT price FROM task WHERE email = '{}';''').format(i)).fetchall()
+            Des4 = cursor_db.execute(('''SELECT description FROM task WHERE email = '{}';''').format(i)).fetchall()
+            Des5 = cursor_db.execute(('''SELECT information FROM task WHERE email = '{}';''').format(i)).fetchall()
+            Des6 = cursor_db.execute(('''SELECT contact FROM task WHERE email = '{}';''').format(i)).fetchall()
+            Des7 = cursor_db.execute(('''SELECT information_meneger FROM task WHERE email = '{}';''').format(i)).fetchall()
+            full_name = []
+            des1 = []
+            des2 = []
+            des3 = []
+            des4 = []
+            des5 = []
+            des6 = []
+            des7 = []
+            arr = []
+            pre_obj = []
+            for j in Full_name:
+                full_name.append(j[0])
+            for j in Des1:
+                des1.append(j[0])
+            for j in Des2:
+                des2.append(j[0])
+            for j in Des3:
+                des3.append(j[0])
+            for j in Des4:
+                des4.append(j[0])
+            for j in Des5:
+                des5.append(j[0])
+            for j in Des6:
+                des6.append(j[0])
+            for j in Des7:
+                des7.append(j[0])  
+            print('full name', full_name)
+                  
+        for i in set(des_hi):
             # получаю описание по взятому email-у
             Des1 = cursor_db.execute(('''SELECT des FROM history WHERE email = '{}';''').format(i)).fetchall()
             #  получаю цену по взятому email-у
@@ -552,10 +595,12 @@ def account():
             with open('static/animation/temp1.json', 'w') as f:
                 # записывает получившийся словарь в json
                 json.dump(obj, f, indent=2)
+            f.close()
         if summ == 0:
             summ = 'Вы бомж'
+        cursor_db.close()
         db_lp.close()
-    return render_template('account.html', Log=Log, Sum=summ, Email=session['email'])
+    return render_template('account.html', Log=fio, Sum=summ, Email=session['email'])
 
 
 @app.route('/')
