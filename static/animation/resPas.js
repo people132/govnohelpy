ce1b01c912538dcd8b15e4567f78491b6dad4fc2
TEaMeNT <aleksandrovv40@gmail.com>
2023-02-26 19:39:13 +0300

    beta_reset_0.5

--- static/animation/resPas.js
+++ static/animation/resPas.js
@@ -1,82 +1,3 @@
-if(window.location.href == 'http://127.0.0.1:5000/re'){
-    window.location.replace(window.location.href + "#Pas")
-}
-const form  = document.getElementsByTagName('form')[1]; // форма для смены ПОЧТЫ!!
-
-const email = document.getElementById('email');
-const emailError = document.querySelector('#email + span.error');
-const newMail = document.getElementById('newMail');
-const newMailError = document.querySelector('#newMail + span.error');
-const password = document.getElementById('password');
-const passwordError = document.querySelector('#password + span.error');
-
-email.addEventListener('input', function (event) { // realtime валидатор
-    if (email.validity.valid) { // почта
-        emailError.textContent = '';
-        emailError.className = 'error';
-    } else {
-        showErrorEmail()
-    }
-});
-newMail.addEventListener('input', function (event) { // realtime валидатор
-    if (newMail.validity.valid) { // почта Снова
-        newMailError.textContent = '';
-        newMailError.className = 'error';
-    } else {
-        showErrorNewMail()
-    }
-});
-password.addEventListener('input', function (event) { // realtime валидатор
-    if (password.validity.valid) { // пароль
-        passwordError.textContent = '';
-        passwordError.className = 'error';
-    } else {
-        showErrorPassword()
-    }
-});
-form.addEventListener('submit', function (event) { // Валидатор на кнопку
-    if(!email.validity.valid) { // почта
-        showErrorEmail()
-        event.preventDefault();
-    }
-    if(!newMail.validity.valid) { // почта снова
-        showErrorNewMail()
-        event.preventDefault();
-    }
-    if(!password.validity.valid) { // пароль
-        showErrorPassword()
-        event.preventDefault();
-    }
-});
-function showErrorEmail() {
-    if(email.validity.valueMissing) { // почта
-        emailError.textContent = 'Это обязательное поле';
-    } else if(email.validity.typeMismatch) {
-        emailError.textContent = 'Это не похоже на почту';
-    } else if(email.validity.tooShort) {
-        emailError.textContent = `Почта должна содержать хотябы ${ email.minLength } символов. Вы ввели ${ email.value.length }.`;
-    }
-    emailError.className = 'error active';
-}
-function showErrorNewMail() {
-    if(newMail.validity.valueMissing) { // почта снова
-        newMailError.textContent = 'Это обязательное поле';
-    } else if(newMail.validity.typeMismatch) {
-        newMailError.textContent = 'Это не похоже на почту';
-    } else if(newMail.validity.tooShort) {
-        newMailError.textContent = `Почта должна содержать хотябы ${ newMail.minLength } символов. Вы ввели ${ newMail.value.length }.`;
-    }
-    newMailError.className = 'error active';
-}
-function showErrorPassword() {
-    if(password.validity.valueMissing) { // пароль
-        passwordError.textContent = 'Это обязательное поле';
-    } else if(password.parentNode) {
-        passwordError.textContent = 'Длина пароля должна быть от 8 до 30 символов, включать одну заглавную букву, один символ и одну цифру. Содержать только латинские буквы';
-    }
-    passwordError.className = 'error active';
-}
-
 const Pform  = document.getElementsByTagName('form')[0]; // форма для смены ПАРОЛЯ!!
 
 const Pemail = document.getElementById('Pemail');
@@ -177,20 +98,6 @@ function showErrorNewPassword(){
     newPasswordError.className = 'error active';
 }
 
-
-
-
-var left_tab = document.getElementsByClassName("nav-link")[0];
-var right_tab = document.getElementsByClassName("nav-link")[1];
-left_tab.classList.add("active_tab");
-right_tab.classList.remove("active_tab");
-left_tab.onclick = function(){
-    left_tab.classList.add("active_tab");
-    right_tab.classList.remove("active_tab");
-    document.getElementsByClassName("head")[0].textContent = "Изменение пароля";
-}
-right_tab.onclick = function(){
-    right_tab.classList.add("active_tab");
-    left_tab.classList.remove("active_tab");
-    document.getElementsByClassName("head")[0].textContent = "Изменение почты";
-}
+document.getElementsByClassName("nav-link")[1].onclick = function() {
+    window.location = ("resMail");
+};
